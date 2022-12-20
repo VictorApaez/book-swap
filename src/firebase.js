355964a26a03ff6036b783677e27eb1fd88b326b
Victor Apaez <vic.apaez@gmail.com>
2022-12-20 13:28:39 -0500

    testing

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA_hRBQVM8wj_bg2KunHLbBQt1CkZoIsH8",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "challenge-cdee6.firebaseapp.com",
   projectId: "challenge-cdee6",
   storageBucket: "challenge-cdee6.appspot.com",
   messagingSenderId: "61179256741",
   appId: "1:61179256741:web:7e1892b281d0a04929124c",
-  measurementId: "G-RZLLWJXRFE"
+  measurementId: "G-RZLLWJXRFE",
 };
 
 // Use this to initialize the firebase App
