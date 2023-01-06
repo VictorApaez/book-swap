796d1e998b9b348acf705aa5b9b48ea34c4773d5
Victor Apaez <vic.apaez@gmail.com>
2023-01-06 14:54:03 -0500

    using dispatch to update state with selected subject

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import "../styles/CategoryList.css";
+import { useDispatch } from "react-redux";
+import { addToShowBooks } from "../store/index.js";
+import { getBookBySubject } from "../services/subject";
 
 function CategoryList({ category }) {
+  const dispatch = useDispatch();
+  const list = useRef();
+
+  function showList() {
+    if (list.current.style.maxHeight === "0px") {
+      list.current.style.maxHeight = "3000px";
+    } else {
+      list.current.style.maxHeight = "0px";
+    }
+  }
+
+  async function handleGenreClick(genre) {
+    let res = await getBookBySubject(genre);
+    const result = {
+      books: res.items,
+      name: genre,
+    };
+    dispatch(addToShowBooks(result));
+  }
   return (
-    <div>
-      <p>{category.name}</p>
-      <ul>
+    <div className="category-list">
+      <div className="category-list-name">
+        <p onClick={showList}>{category.name}</p>
+        <span className="material-symbols-outlined">expand_less</span>
+      </div>
+      <ul className="category-list-all" ref={list}>
         {category.genres.map((genre, i) => {
-          return <li>{genre}</li>;
+          return (
+            <li key={i} onClick={() => handleGenreClick(genre)}>
+              {genre}
+            </li>
+          );
         })}
       </ul>
     </div>
