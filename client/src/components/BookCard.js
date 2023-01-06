c508595ea0f8bc34c009e3ccb085ca8dc7c2676d
Victor Apaez <vic.apaez@gmail.com>
2023-01-06 14:52:33 -0500

    had to add optional chaining because not all books contain authors and thumbnails

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -3,10 +3,12 @@ import "../styles/BookCard.css";
 function BookCard({ data }) {
   return (
     <div className="book-card">
-      <img src={data.volumeInfo.imageLinks.thumbnail} />
+      <img src={data?.volumeInfo?.imageLinks?.thumbnail} />
       <div className="book-card-info">
-        <h4>{data.volumeInfo.title}</h4>
-        <p>{data.volumeInfo.authors[0]}</p>
+        <h4>{data?.volumeInfo?.title}</h4>
+        {data?.volumeInfo?.authors?.map((author, i) => {
+          return <p key={i}>{author}</p>;
+        })}
         <button>More info</button>
       </div>
     </div>
