a9f9ca9bfb99c86f7a5f248d38fcfe01ad0dc32c
Victor Apaez <vic.apaez@gmail.com>
2023-01-06 23:29:00 -0500

    added drop down icon and changing subject's background color when clicked

--- client/src/components/CategoryList.js
+++ client/src/components/CategoryList.js
@@ -7,12 +7,18 @@ import { getBookBySubject } from "../services/subject";
 function CategoryList({ category }) {
   const dispatch = useDispatch();
   const list = useRef();
+  const showIcon = useRef();
+  const nameContainer = useRef();
 
   function showList() {
     if (list.current.style.maxHeight === "0px") {
       list.current.style.maxHeight = "3000px";
+      showIcon.current.style.transform = "rotate(180deg)";
+      nameContainer.current.style.backgroundColor = "rgb(44, 44, 44)";
     } else {
       list.current.style.maxHeight = "0px";
+      showIcon.current.style.transform = "rotate(0deg)";
+      nameContainer.current.style.backgroundColor = "";
     }
   }
 
@@ -26,11 +32,20 @@ function CategoryList({ category }) {
   }
   return (
     <div className="category-list">
-      <div className="category-list-name">
-        <p onClick={showList}>{category.name}</p>
-        <span className="material-symbols-outlined">expand_less</span>
+      <div
+        ref={nameContainer}
+        onClick={showList}
+        className="category-list-name"
+      >
+        <p>{category.name}</p>
+        <span
+          ref={showIcon}
+          className="material-symbols-outlined category-list-icon"
+        >
+          expand_less
+        </span>
       </div>
-      <ul className="category-list-all" ref={list}>
+      <ul className="category-list-all" ref={list} style={{ maxHeight: "0px" }}>
         {category.genres.map((genre, i) => {
           return (
             <li key={i} onClick={() => handleGenreClick(genre)}>
