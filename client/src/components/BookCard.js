fc7ef32f640a23ade49491d67f396bfaf740c0d0
Victor Apaez <vic.apaez@gmail.com>
2023-01-06 23:27:15 -0500

    updated default image for books

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/BookCard.css";
 function BookCard({ data }) {
+  const thumbnail =
+    data?.volumeInfo?.imageLinks?.thumbnail ||
+    "https://islandpress.org/sites/default/files/default_book_cover_2015.jpg";
   return (
     <div className="book-card">
-      <img src={data?.volumeInfo?.imageLinks?.thumbnail} />
+      <img src={thumbnail} />
       <div className="book-card-info">
         <h4>{data?.volumeInfo?.title}</h4>
-        {data?.volumeInfo?.authors?.map((author, i) => {
-          return <p key={i}>{author}</p>;
-        })}
+        <div className="book-card-authors">
+          {data?.volumeInfo?.authors?.map((author, i) => {
+            return <p key={i}>{author}</p>;
+          })}
+        </div>
+
         <button>More info</button>
       </div>
     </div>
