e83aeeed148ecd4b6c382c4ce5efe781bb331333
Victor Apaez <vic.apaez@gmail.com>
2023-01-10 20:33:17 -0500

    scrolling container back to top when clicking next page and added state to keep track of loading cards

--- client/src/components/BooksContainer.js
+++ client/src/components/BooksContainer.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getBookBySubject } from "../services/subject";
 import BookCard from "./BookCard";
 import "../styles/BooksContainer.css";
 import { useDispatch, useSelector } from "react-redux";
 
 function BooksContainer() {
+  const BooksContainer = useRef();
   const [pageNum, setPageNum] = useState(0);
   const [loadingPage, setLoadingPage] = useState(false);
   const subject = useSelector((state) => {
@@ -13,28 +14,34 @@ function BooksContainer() {
   const [books, setBooks] = useState();
   useEffect(async () => {
     setPageNum(0);
+    setLoadingPage(true);
     let res = await getBookBySubject(subject, 0);
+    setLoadingPage(false);
     setBooks(res.items);
   }, [subject]);
 
   useEffect(async () => {
+    setLoadingPage(true);
     let res = await getBookBySubject(subject, pageNum);
+    setLoadingPage(false);
     setBooks(res.items);
   }, [pageNum]);
 
   function handlePageDown() {
     if (pageNum > 0 && !loadingPage) setPageNum(pageNum - 20);
+    BooksContainer.current.scrollTop = 0;
   }
   function handlePageUp() {
     if (!loadingPage) setPageNum(pageNum + 20);
+    BooksContainer.current.scrollTop = 0;
   }
   return (
-    <div className="books-wrapper">
+    <div className="books-wrapper" ref={BooksContainer}>
       <h1>{subject && subject.toUpperCase()}</h1>
       <div className="books-container">
         {books &&
           books.map((book, i) => {
-            return <BookCard key={i} data={book} />;
+            return <BookCard key={i} data={book} loadingPage={loadingPage} />;
           })}
       </div>
       <div className="page-number-container">
