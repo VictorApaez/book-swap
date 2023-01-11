2581d4dab2197d9c69c33f01247f86330b506c9b
Victor Apaez <vic.apaez@gmail.com>
2023-01-10 20:35:49 -0500

    changed the root variable names

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,13 +5,15 @@ import {
   addToShowBooks,
   setSubject,
 } from "./slices/showBooks.js";
+import { themeReducer, changeTheme } from "./slices/theme.js";
 import { resetState } from "./actions";
 
 const store = configureStore({
   reducer: {
     showBooks: showBooksReducer,
+    theme: themeReducer,
   },
 });
 
 // this is so all exports are from one file location when you import them in your components
-export { store, addToShowBooks, resetState, setSubject };
+export { store, addToShowBooks, changeTheme, resetState, setSubject };
