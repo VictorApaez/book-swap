0250bb53583406a2981759f930d281f4b0e3a510
Victor Apaez <vic.apaez@gmail.com>
2023-01-25 16:01:43 -0500

    removed unused imports

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,28 +1,16 @@
 // https://redux-toolkit.js.org/introduction/getting-started
 import { configureStore } from "@reduxjs/toolkit";
-import {
-  showBooksReducer,
-  addToShowBooks,
-  setSubject,
-} from "./slices/showBooks.js";
+
 import { themeReducer, changeTheme } from "./slices/theme.js";
 import { resetState } from "./actions";
 import { updateBooks, booksReducer } from "./slices/Books.js";
 
 const store = configureStore({
   reducer: {
-    showBooks: showBooksReducer,
     theme: themeReducer,
     books: booksReducer,
   },
 });
 
 // this is so all exports are from one file location when you import them in your components
-export {
-  store,
-  addToShowBooks,
-  changeTheme,
-  resetState,
-  setSubject,
-  updateBooks,
-};
+export { store, changeTheme, resetState, updateBooks };
