eeda16621c2e0b7ecbd25fdc4a5060701ddbc358
Victor Apaez <vic.apaez@gmail.com>
2023-01-25 16:06:29 -0500

    prop drilling loading and page number state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import "../styles/Home.css";
 import BooksContainer from "../components/BooksContainer";
 
-function Home() {
+function Home({ setLoadingPage, loadingPage, setPageNum, pageNum }) {
   return (
     <div className="home">
-      <BooksContainer></BooksContainer>
+      <BooksContainer
+        setLoadingPage={setLoadingPage}
+        loadingPage={loadingPage}
+        setPageNum={setPageNum}
+        pageNum={pageNum}
+      />
     </div>
   );
 }
