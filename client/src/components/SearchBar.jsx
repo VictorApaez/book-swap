7969e919f19a17cd43043240a905437c92cbde81
Victor Apaez <vic.apaez@gmail.com>
2023-01-25 16:12:25 -0500

    implemented search books by any words

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { useRef } from "react";
-import { getBooks } from "../services/subject";
+import { getBooksByAny } from "../services/books.js";
+import { useDispatch } from "react-redux";
+import { updateBooks } from "../store/index.js";
 
-export function SearchBar({}) {
+export function SearchBar({
+  setLoadingPage,
+  setPageNum,
+  setToggleAside,
+  toggleAside,
+}) {
   const searchInput = useRef();
+  const dispatch = useDispatch();
 
   async function handleForm(e) {
     e.preventDefault();
-    // const res = await getBooks(searchInput.current.value);
+    setLoadingPage(true);
+    setPageNum(0);
+    setToggleAside(!toggleAside);
+    const res = await getBooksByAny(searchInput.current.value);
+    setLoadingPage(false);
+    if (res.totalItems > 0) {
+      dispatch(
+        updateBooks({
+          subject: searchInput.current.value,
+          data: res.items,
+          customSearch: true,
+        })
+      );
+    }
     searchInput.current.value = "";
   }
   return (
