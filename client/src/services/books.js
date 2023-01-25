25f32bd0986acb0ded467cf7e516a91d417b56db
Victor Apaez <vic.apaez@gmail.com>
2023-01-25 16:02:57 -0500

    service for fetching books by subject and by any

---
+++ client/src/services/books.js
@@ -0,0 +1,23 @@
+import api from "./apiConfig";
+
+export const getBookBySubject = async (subject, index) => {
+  console.log(index);
+  try {
+    const res = await api.get(`/subject/${subject}/${index}`);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getBooksByAny = async (input, index = 0) => {
+  try {
+    const query = input.split(" ").join("%20");
+    const res = await api.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${index}`
+    );
+    return res.data;
+  } catch (e) {
+    throw e;
+  }
+};
