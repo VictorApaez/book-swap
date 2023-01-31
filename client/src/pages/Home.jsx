b38d03948433a8fb58cc99bbc5ef9a153c4878c8
Victor Apaez <vic.apaez@gmail.com>
2023-01-31 10:45:24 -0500

    passing in scrollHeroTop function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,13 @@ import React, { useState } from "react";
 import "../styles/Home.css";
 import BooksContainer from "../components/BooksContainer";
 
-function Home({ setLoadingPage, loadingPage, setPageNum, pageNum }) {
+function Home({
+  setLoadingPage,
+  loadingPage,
+  setPageNum,
+  pageNum,
+  scrollHeroTop,
+}) {
   return (
     <div className="home">
       <BooksContainer
@@ -10,6 +16,7 @@ function Home({ setLoadingPage, loadingPage, setPageNum, pageNum }) {
         loadingPage={loadingPage}
         setPageNum={setPageNum}
         pageNum={pageNum}
+        scrollHeroTop={scrollHeroTop}
       />
     </div>
   );
