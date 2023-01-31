36cab54332de88d1b85a7e849c898e324fe80b09
Victor Apaez <vic.apaez@gmail.com>
2023-01-31 10:37:52 -0500

    created main landing page and using random images for carousels and displaying search bar component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import { CarouselContainer } from "./CarouselContainer";
+import React, { useState, useEffect } from "react";
 import "../styles/Main.css";
+import "../styles/Carousel.css";
+import { SearchBar } from "../components/SearchBar";
+import LogoAnimation from "./LogoAnimation";
 
 function Main() {
+  const images = [
+    "https://books.google.com/books/content?id=oLr2iVoNENAC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=3Hr5ONX-2G8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    "https://books.google.com/books/content?id=FLFPCqyuU_EC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=Mht4vgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=gHcEDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    "https://books.google.com/books/content?id=RQDiooLMQq8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    "https://books.google.com/books/content?id=eEvlswEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=EL_R7Tiig74C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    "https://books.google.com/books/content?id=L2ylzQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=QNBrzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=L2ylzQEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+    "https://books.google.com/books/content?id=QNBrzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+  ];
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 2600);
+  }, []);
+
+  let animate = {
+    opacity: isAnimating ? "1" : "0",
+    zIndex: isAnimating ? "3" : "1",
+  };
   return (
-    <div className="main">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
+    <div className="main-wrapper">
+      <div className="main-overlay ">
+        <div className="main-overlay-bg-animation" style={animate}></div>
+        <LogoAnimation></LogoAnimation>
+        <div className="main-search-bar">
+          <SearchBar></SearchBar>
+        </div>
+      </div>
+      <CarouselContainer images={images} />
     </div>
   );
 }
