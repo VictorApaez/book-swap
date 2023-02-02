231cfbf0ad47006a6514c5b7d16bc1ed61fb0ba8
Victor Apaez <vic.apaez@gmail.com>
2023-02-02 11:02:51 -0500

    removed prop drilling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,22 +2,10 @@ import React, { useState } from "react";
 import "../styles/Home.css";
 import BooksContainer from "../components/BooksContainer";
 
-function Home({
-  setLoadingPage,
-  loadingPage,
-  setPageNum,
-  pageNum,
-  scrollHeroTop,
-}) {
+function Home({ scrollHeroTop }) {
   return (
     <div className="home">
-      <BooksContainer
-        setLoadingPage={setLoadingPage}
-        loadingPage={loadingPage}
-        setPageNum={setPageNum}
-        pageNum={pageNum}
-        scrollHeroTop={scrollHeroTop}
-      />
+      <BooksContainer scrollHeroTop={scrollHeroTop} />
     </div>
   );
 }
