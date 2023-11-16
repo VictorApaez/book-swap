b93982882afa819e0157d299f7ab4e9c5847eaee
VictorApaez <vic.apaez@gmail.com>
2023-11-16 18:25:43 -0500

    upated api endpoint

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const url =
-  "https://bookswap.up.railway.app/api" || "http://localhost:5000/api";
+  "https://book-swap-sepia.vercel.app/" || "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: url,
