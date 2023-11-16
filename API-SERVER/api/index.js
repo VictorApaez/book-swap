0d91e30179c1354472b83a995a7ae7e10fdc21d9
VictorApaez <vic.apaez@gmail.com>
2023-11-16 18:20:14 -0500

    added cofig for vercel

---
+++ API-SERVER/api/index.js
@@ -0,0 +1,2 @@
+import { app } from "../index";
+export default app;
--- API-SERVER/index.js
+++ API-SERVER/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+export const app = express();
 
 app.use(cors());
 app.use("/api", require("./routes"));
